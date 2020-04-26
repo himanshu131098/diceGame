5bfbe3df96bdf69ae00c89ec698447bdfc408e43
himanshu131098 <63089989+himanshu131098@users.noreply.github.com>
2020-04-26 12:18:41 +0530

    Final Version

--- index.js
+++ index.js
@@ -3,55 +3,33 @@ button.addEventListener("click", game);
 
 function game() {
 
-  var randomNumber1 = Math.random();
-  randomNumber1 = randomNumber1 * 6;
-  randomNumber1 = Math.floor(randomNumber1) + 1;
+  // Player One Play
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+  var randomImageSource1 = "images/" + randomDiceImage1;
+  var image1 = document.querySelector(".dice1");
+  image1.setAttribute("src", randomImageSource1);
 
-  if (randomNumber1 == 1) {
-    document.querySelector(".name1 img").setAttribute("src", "images/dice1.png");
-  } else if (randomNumber1 == 2) {
-    document.querySelector(".name1 img").setAttribute("src", "images/dice2.png");
-  } else if (randomNumber1 == 3) {
-    document.querySelector(".name1 img").setAttribute("src", "images/dice3.png");
-  } else if (randomNumber1 == 4) {
-    document.querySelector(".name1 img").setAttribute("src", "images/dice4.png");
-  } else if (randomNumber1 == 5) {
-    document.querySelector(".name1 img").setAttribute("src", "images/dice5.png");
-  } else {
-    document.querySelector(".name1 img").setAttribute("src", "images/dice6.png");
-  }
-
-  var randomNumber2 = Math.random();
-  randomNumber2 = randomNumber2 * 6;
-  randomNumber2 = Math.floor(randomNumber2) + 1;
-
-  if (randomNumber2 == 1) {
-    document.querySelector(".name2 img").setAttribute("src", "images/dice1.png");
-  } else if (randomNumber2 == 2) {
-    document.querySelector(".name2 img").setAttribute("src", "images/dice2.png");
-  } else if (randomNumber2 == 3) {
-    document.querySelector(".name2 img").setAttribute("src", "images/dice3.png");
-  } else if (randomNumber2 == 4) {
-    document.querySelector(".name2 img").setAttribute("src", "images/dice4.png");
-  } else if (randomNumber2 == 5) {
-    document.querySelector(".name2 img").setAttribute("src", "images/dice5.png");
-  } else {
-    document.querySelector(".name2 img").setAttribute("src", "images/dice6.png");
-  }
+  // Player Two Play
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+  var randomImageSource2 = "images/" + randomDiceImage2;
+  var image2 = document.querySelector(".dice2");
+  image2.setAttribute("src", randomImageSource2);
 
   var winner = document.querySelector("h1");
 
   if (randomNumber1 > randomNumber2) {
     winner.innerHTML = "Bachuu Wins";
-    document.querySelector(".p1").style.visibility="hidden";
-    document.querySelector(".p2").style.visibility="visible";
+    document.querySelector(".p1").style.visibility = "hidden";
+    document.querySelector(".p2").style.visibility = "visible";
   } else if (randomNumber1 < randomNumber2) {
     winner.innerHTML = "Buddy Wins";
-    document.querySelector(".p1").style.visibility="visible";
-    document.querySelector(".p2").style.visibility="hidden";
+    document.querySelector(".p1").style.visibility = "visible";
+    document.querySelector(".p2").style.visibility = "hidden";
   } else {
     winner.innerHTML = "Draw!";
-    document.querySelector(".p1").style.visibility="visible";
-    document.querySelector(".p2").style.visibility="visible";
+    document.querySelector(".p1").style.visibility = "visible";
+    document.querySelector(".p2").style.visibility = "visible";
   }
 }
